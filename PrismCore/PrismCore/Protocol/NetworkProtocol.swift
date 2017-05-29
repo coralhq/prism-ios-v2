172037c70789615524d3acd10db614ec6415d04d
Nanang Rafsanjani <jukiginanjar@users.noreply.github.com>
2017-05-29 13:09:28 +0700

    [SDK-30] Network protocol (#3)
    * create protocol for network utils
    
    * rename Networking to Network
    
    * remove setMQTTDelegate from protocol, not needed for mocking
    
    * add missing file to the project

---
+++ PrismCore/PrismCore/Protocol/NetworkProtocol.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkProtocol.swift
+//  PrismCore
+//
+//  Created by Nanang Rafsanjani on 5/24/17.
+//  Copyright © 2017 PrismApp. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkProtocol {
+    func requestRawResult<T: Mappable>(endPoint: EndPoint, mapToObject: T.Type, completionHandler: @escaping (([String: Any]?, Error?) -> ()))
+    
+    func request<T: Mappable>(endPoint: EndPoint, mapToObject: T.Type, completionHandler: @escaping HTTPRequestResult)
+    
+    func connectToBroker(username: String, password: String, completionHandler: @escaping ((Bool, Error) -> ()))
+    
+    func subscribeToTopic(topic: String, completionHandler: @escaping ((Bool, Error) -> ()))
+    
+    func publishMessage(topic: String, message: Message, completionHandler: @escaping (Message?, Error?) -> ())
+}

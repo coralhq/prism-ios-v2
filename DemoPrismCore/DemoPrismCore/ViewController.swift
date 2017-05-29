67770d59510c340e2503bf4c2856dc2fa45ed737
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-05-29 12:39:35 +0700

    refine demo app to simulate upload image

--- DemoPrismCore/DemoPrismCore/ViewController.swift
+++ DemoPrismCore/DemoPrismCore/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     @IBOutlet var conenctButton:UIButton!
     
     let prismCore = PrismCore(environment: EnvironmentType.Sandbox, merchantID: "62ccf49f-0386-49a3-858c-70c98a9dc4fc")
+    var connectResponse: ConnectResponse?
+    var createConversationResponse: CreateConversationResponse?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,22 +35,40 @@ class ViewController: UIViewController {
         
         guard let name = nameTextField.text, let identifier = idTextField.text else { return }
         prismCore.visitorConnect(visitorName: name, userID: identifier) { [weak self] (connectResponse, error) in
-            //            DispatchQueue.main.async(){
-            sender.setTitle("Connect", for: .normal)
-            self?.view.isUserInteractionEnabled = true
             
             if let error = error {
                 print(error.localizedDescription)
             } else if let response = connectResponse {
-                self?.prismCore.createConversation(visitorName: response.visitor.name, token: "Bearer \(response.oAuth.accessToken)") { (createConversationResponse, error) in
+                self?.connectResponse = response
+                
+                self?.prismCore.createConversation(visitorName: response.visitor.name, token: response.oAuth.accessToken) { (createConversationResponse, error) in
+                    
+                    sender.setTitle("Connect", for: .normal)
+                    self?.view.isUserInteractionEnabled = true
+                    
                     if let error = error {
                         print(error)
                     } else if let response = createConversationResponse {
+                        self?.createConversationResponse = response
+                        
                         print("success \(response.conversation)")
                     }
                 }
             }
-            //            }
+        }
+    }
+    
+    @IBAction func uploadPressed(sender:UIButton) -> Void {
+        if let connect = connectResponse, let conversation = createConversationResponse {
+            prismCore.getAttachmentURL(filename: "test-persebaya.jpg", conversationID: conversation.conversation.id, token: connect.oAuth.accessToken, completionHandler: { [weak self] (url, error) in
+                let image = #imageLiteral(resourceName: "persebaya")
+                guard let imageData = UIImagePNGRepresentation(image), let imageURL = url?.uploadURL else { return }
+                self?.prismCore.uploadAttachment(with: imageData, url: imageURL, completionHandler: { (response, error) in
+                    if let res = response {
+                        print("response \(res)")
+                    }
+                })
+            })
         }
     }
 }

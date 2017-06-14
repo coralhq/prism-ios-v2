373f680bab45b8a7e3f4d9a99beb40f3074d3cd4
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-06-15 06:11:31 +0700

    conform to NSCoding so it can be serialized

--- PrismUI/PrismUI/Models/InputFormSettings.swift
+++ PrismUI/PrismUI/Models/InputFormSettings.swift
@@ -8,34 +8,74 @@
 
 import Foundation
 
-public class InputForm: NSObject {
+public class InputForm: NSObject, NSCoding {
+    struct Keys {
+        static var show = "show"
+        static var required = "required"
+    }
+    
     var show: Bool
     var required: Bool
     
     init(dict: [String: Any]) {
-        self.show = dict["show"] as! Bool
-        self.required = dict["required"] as! Bool
+        self.show = dict[Keys.show] as! Bool
+        self.required = dict[Keys.required] as! Bool
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        self.show = aDecoder.decodeBool(forKey: Keys.show)
+        self.required = aDecoder.decodeBool(forKey: Keys.required)
+    }
+    
+    public func encode(with aCoder: NSCoder) {
+        aCoder.encode(show, forKey: Keys.show)
+        aCoder.encode(required, forKey: Keys.required)
     }
 }
 
-public class InputFormSettings: NSObject {
+public class InputFormSettings: NSObject, NSCoding {
+    struct Keys {
+        static var enabled = "enabled"
+        static var username = "name"
+        static var email = "email"
+        static var phoneNumber = "phone"
+        static var inputFormField = "input_form_field"
+        static var inputForm = "input_form"
+        static var widget = "widget"
+        static var publicData = "public"
+    }
+    
     var enabled: Bool
     var username: InputForm
     var email: InputForm
     var phoneNumber: InputForm
     
     init?(settings: [String: Any]) {
-        guard let publicData = settings["public"] as? [String: Any],
-            let widget = publicData["widget"] as? [String: Any],
-            let formEnabled = widget["input_form"] as? String,
-            let formDict = widget["input_form_field"] as? [String: Any],
-            let nameDict = formDict["name"] as? [String: Any],
-            let emailDict = formDict["email"] as? [String: Any],
-            let phoneDict = formDict["phone"] as? [String: Any] else { return nil }
+        guard let publicData = settings[Keys.publicData] as? [String: Any],
+            let widget = publicData[Keys.widget] as? [String: Any],
+            let formEnabled = widget[Keys.inputForm] as? String,
+            let formDict = widget[Keys.inputFormField] as? [String: Any],
+            let nameDict = formDict[Keys.username] as? [String: Any],
+            let emailDict = formDict[Keys.email] as? [String: Any],
+            let phoneDict = formDict[Keys.phoneNumber] as? [String: Any] else { return nil }
         
         enabled = formEnabled == "ENABLED"
         username = InputForm(dict: nameDict)
         email = InputForm(dict: emailDict)
         phoneNumber = InputForm(dict: phoneDict)
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        self.enabled = aDecoder.decodeBool(forKey: Keys.enabled)
+        self.username = aDecoder.decodeObject(forKey: Keys.username) as! InputForm
+        self.email = aDecoder.decodeObject(forKey: Keys.email) as! InputForm
+        self.phoneNumber = aDecoder.decodeObject(forKey: Keys.phoneNumber) as! InputForm
+    }
+    
+    public func encode(with aCoder: NSCoder) {
+        aCoder.encode(enabled, forKey: Keys.enabled)
+        aCoder.encode(username, forKey: Keys.username)
+        aCoder.encode(email, forKey: Keys.email)
+        aCoder.encode(phoneNumber, forKey: Keys.phoneNumber)
     }
 }
--- PrismUI/PrismUI/Models/PrismCredential.swift
+++ PrismUI/PrismUI/Models/PrismCredential.swift
@@ -9,12 +9,27 @@
 import Foundation
 import PrismCore
 
-class PrismCredential: NSObject {
+class PrismCredential: NSObject, NSCoding {
+    struct Keys {
+        static var username = "username"
+        static var password = "password"
+    }
+    
     var username: String
     var password: String
     
     init(connectResponse: ConnectResponse) {
         username = connectResponse.mqtt.username
         password = connectResponse.mqtt.password
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        username = aDecoder.decodeObject(forKey: Keys.username) as! String
+        password = aDecoder.decodeObject(forKey: Keys.password) as! String
+    }
+    
+    public func encode(with aCoder: NSCoder) {
+        aCoder.encode(username, forKey: Keys.username)
+        aCoder.encode(password, forKey: Keys.password)
     }
 }

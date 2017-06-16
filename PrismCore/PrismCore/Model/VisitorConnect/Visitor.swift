f2a04462f59a975e8f8442ee702a4a7adb062f4f
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-06-16 08:38:58 +0700

    rollback visitor

--- PrismCore/PrismCore/Model/VisitorConnect/Visitor.swift
+++ PrismCore/PrismCore/Model/VisitorConnect/Visitor.swift
@@ -8,17 +8,8 @@
 
 import Foundation
 
-struct VisitorKeys {
-    static let id = "id"
-    static let name = "name"
-    static let merchantID = "merchant_id"
-    static let appName = "app_name"
-    static let channelName = "channel_name"
-    static let channelUserID = "channel_user_id"
-}
-
 //TODO: some properties might unused, re-check later
-public class Visitor : Mappable, NSCoding {
+public class Visitor : Mappable {
     public var id: String
     public var name: String
     public var merchantID: String
@@ -27,43 +18,16 @@ public class Visitor : Mappable, NSCoding {
     public var channelUserID: String
     
     required public init?(dictionary: [String: Any]?) {
-        guard let id = dictionary?[VisitorKeys.id] as? String,
-            let name = dictionary?[VisitorKeys.name] as? String,
-            let merchantID = dictionary?[VisitorKeys.merchantID] as? String,
-            let appName = dictionary?[VisitorKeys.appName] as? String,
-            let channelName = dictionary?[VisitorKeys.channelName] as? String,
-            let channelUserID = dictionary?[VisitorKeys.channelUserID] as? String else {
+        guard let id = dictionary?["id"] as? String,
+            let name = dictionary?["name"] as? String,
+            let merchantID = dictionary?["merchant_id"] as? String,
+            let appName = dictionary?["app_name"] as? String,
+            let channelName = dictionary?["channel_name"] as? String,
+            let channelUserID = dictionary?["channel_user_id"] as? String else {
                 return nil
         }
         
         self.id = id
-        self.name = name
-        self.merchantID = merchantID
-        self.appName = appName
-        self.channelName = channelName
-        self.channelUserID = channelUserID
-    }
-    
-    public func encode(with aCoder: NSCoder) {
-        aCoder.encode(id, forKey: VisitorKeys.id)
-        aCoder.encode(name, forKey: VisitorKeys.name)
-        aCoder.encode(merchantID, forKey: VisitorKeys.merchantID)
-        aCoder.encode(appName, forKey: VisitorKeys.appName)
-        aCoder.encode(channelName, forKey: VisitorKeys.channelName)
-        aCoder.encode(channelUserID, forKey: VisitorKeys.channelUserID)
-    }
-    
-    public required convenience init?(coder aDecoder: NSCoder) {
-        self.init(coder: aDecoder)
-        guard let id = aDecoder.decodeObject(forKey: VisitorKeys.id) as? String,
-            let name = aDecoder.decodeObject(forKey: VisitorKeys.name) as? String,
-            let merchantID = aDecoder.decodeObject(forKey: VisitorKeys.merchantID) as? String,
-            let appName = aDecoder.decodeObject(forKey: VisitorKeys.appName) as? String,
-            let channelName = aDecoder.decodeObject(forKey: VisitorKeys.channelName) as? String,
-            let channelUserID = aDecoder.decodeObject(forKey: VisitorKeys.channelUserID) as? String else { return }
-        
-        self.id = id
-        self.merchantID = merchantID
         self.name = name
         self.merchantID = merchantID
         self.appName = appName

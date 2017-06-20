e3dbc71669d6a20982b7a11c0201179ad1d13650
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-06-20 10:13:57 +0700

    refine demo

--- DemoPrismCore/DemoPrismCore/AppDelegate.swift
+++ DemoPrismCore/DemoPrismCore/AppDelegate.swift
@@ -19,10 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         window = UIWindow(frame: UIScreen.main.bounds)
-
         let vc = DemoViewController(nibName: "DemoViewController", bundle: nil)
-        let navBar = UINavigationController(rootViewController: vc)
-        window!.rootViewController = navBar
+        window!.rootViewController = vc
         window!.makeKeyAndVisible()
         
         return true

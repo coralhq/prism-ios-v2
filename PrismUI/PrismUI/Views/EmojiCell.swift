fb6cdbe89cb6d283b5736f85863cba014186cb8e
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-06-21 16:36:24 +0700

    implement emoji keyboard

---
+++ PrismUI/PrismUI/Views/EmojiCell.swift
@@ -0,0 +1,19 @@
+//
+//  EmojiCell.swift
+//  PrismUI
+//
+//  Created by Nanang Rafsanjani on 6/20/17.
+//  Copyright © 2017 Prism. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCell: UICollectionViewCell {
+    @IBOutlet var emojiLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

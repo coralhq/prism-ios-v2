59cab52c21d0761e8f7af7a5c85b7dac46af55ee
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-07-05 11:10:38 +0700

    set view in chatcontainer with data from viewModel

--- PrismUI/PrismUI/Cells/ChatCell/Container/ChatContainerView.swift
+++ PrismUI/PrismUI/Cells/ChatCell/Container/ChatContainerView.swift
@@ -14,17 +14,44 @@ enum InfoViewPosition {
     case Left
 }
 
+class ChatInfoView: UIView {
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var statusImageView: UIImageView?
+}
+
 class ChatContainerView: UIView {
-    @IBOutlet var infoView: UIView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var infoView: ChatInfoView!
     @IBOutlet var containerView: UIView!
     @IBOutlet var infoWithContentHSpace: NSLayoutConstraint!
     @IBOutlet var infoWithContentVSpace: NSLayoutConstraint!
+    
+    var viewModel: ChatViewModel! {
+        didSet {
+            infoView.timeLabel.text = viewModel.messageTime
+            if viewModel.cellType == .Out {
+                if viewModel.messageStatus == .sent {
+                    infoView.statusImageView?.image = UIImage(named: "icStatusRead", in: Bundle.prism, compatibleWith: nil)
+                } else {
+                    infoView.statusImageView?.image = UIImage(named: "icStatusSending", in: Bundle.prism, compatibleWith: nil)
+                }
+            }
+            
+            nameLabel.text = viewModel.senderName
+        }
+    }
     
     var chatContentView: ChatContentProtocol? {
         didSet {
             chatContentView?.addTo(view: containerView)
             updateInfoView()
         }
+    }
+
+    static func viewFromNib(with viewModel: ChatViewModel) -> ChatContainerView? {
+        let view: ChatContainerView = self.viewFromNib() as! ChatContainerView
+        view.viewModel = viewModel
+        return view
     }
     
     func updateInfoView() {

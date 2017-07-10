3c7d6bc6866495e42d05c94614e6f5586ad95ff2
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-07-10 17:56:26 +0700

    refine code

--- PrismCore/PrismCore/Model/UploadURL.swift
+++ PrismCore/PrismCore/Model/UploadURL.swift
@@ -12,18 +12,11 @@ open class UploadURL: Mappable {
     public let uploadURL: URL
     
     required public init?(dictionary: [String : Any]?) {
-        if let data = dictionary?["data"] as? [String : Any] {
-            print("data \(data)")
-            if let url = data["upload_url"] {
-                print("url \(url)")
-            }
-        }
         guard let data = dictionary?["data"] as? [String: Any],
             let stringURL = data["upload_url"] as? String
             else {
                 return nil
         }
-        
         self.uploadURL = URL(string: stringURL)!
     }
 }

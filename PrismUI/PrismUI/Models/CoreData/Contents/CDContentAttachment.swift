210cd042796eb9f2451ac67393e3201e204bf024
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-07-10 17:43:02 +0700

    implement editable content

--- PrismUI/PrismUI/Models/CoreData/Contents/CDContentAttachment.swift
+++ PrismUI/PrismUI/Models/CoreData/Contents/CDContentAttachment.swift
@@ -9,17 +9,28 @@
 import UIKit
 import PrismCore
 
+enum AttachmentUploadState: Int {
+    case start = 11
+    case uploading = 22
+    case finished = 33
+}
+
 class CDContentAttachment: ValueTransformer, NSCoding {
     var name: String?
     var mimeType: String?
-    var url: URL?
-    var previewURL: URL?
+    var url: String?
+    var previewURL: String?
+    
+    var uploadState: AttachmentUploadState?
+    var tempAttachment: NSObject?
     
     init(contentAttachment: ContentAttachment) {
         name = contentAttachment.name
         mimeType = contentAttachment.mimeType
         url = contentAttachment.url
         previewURL = contentAttachment.previewURL
+        
+        uploadState = .finished
     }
     
     func encode(with aCoder: NSCoder) {
@@ -27,12 +38,34 @@ class CDContentAttachment: ValueTransformer, NSCoding {
         aCoder.encode(mimeType, forKey: "mime_type")
         aCoder.encode(url, forKey: "url")
         aCoder.encode(previewURL, forKey: "preview_url")
+        aCoder.encode(uploadState?.rawValue, forKey: "upload_state")
+        aCoder.encode(tempAttachment, forKey: "temp_attachment")
     }
     
     required init?(coder aDecoder: NSCoder) {
         name = aDecoder.decodeObject(forKey: "name") as? String
         mimeType = aDecoder.decodeObject(forKey: "mime_type") as? String
-        url = aDecoder.decodeObject(forKey: "url") as? URL
-        previewURL = aDecoder.decodeObject(forKey: "preview_url") as? URL
+        url = aDecoder.decodeObject(forKey: "url") as? String
+        previewURL = aDecoder.decodeObject(forKey: "preview_url") as? String
+        tempAttachment = aDecoder.decodeObject(forKey: "temp_attachment") as? NSObject
+        
+        guard let state = aDecoder.decodeObject(forKey: "upload_state") as? Int else { return }
+        self.uploadState = AttachmentUploadState(rawValue: state)
     }
+}
+
+extension CDContentAttachment: CDContentEditable {
+    func editWithContent(content: MessageContentMappable) {
+        guard let content = content as? ContentAttachment else {
+            return
+        }
+        name = content.name
+        mimeType = content.mimeType
+        url = content.url
+        previewURL = content.previewURL
+    }
+}
+
+protocol CDContentEditable {
+    func editWithContent(content: MessageContentMappable)
 }

8cee5cd7d1b17b3e6d9d8b327f95f92dd8ee5967
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-07-10 17:41:27 +0700

    implement send image message

--- PrismUI/PrismUI/Utils/ChatManager.swift
+++ PrismUI/PrismUI/Utils/ChatManager.swift
@@ -26,7 +26,7 @@ class ChatManager {
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
-    
+
     func connect(completionHandler: @escaping ((Bool, Error?) -> ())) {
         PrismCore.shared.connectToBroker(username: credential.username, password: credential.password) { (success, error) in
             completionHandler(success, error)
@@ -40,33 +40,83 @@ class ChatManager {
     }
     
     func sendMessage(text: String) {
-        guard let content = ContentPlainText(text: text),
-            let message = message(with: content, type: .PlainText) else { return }
+        guard let content = ContentPlainText(text: text) else { return }
+        let message = buildMessage(with: content, type: .PlainText)
         sendMessage(message: message)
     }
     
-    func sendMessage(image: UIImage) {
+    func sendMessage(image: UIImage, imageName: String) {
+        let content = ContentAttachment(name: imageName, mimeType: "image")
+        let message = buildMessage(with: content, type: .Attachment)
         
+        guard let cd = coredata,
+            let cdmessage = coredata?.buildMessage(message: message, status: .pending),
+            let cdcontent = cdmessage.content as? CDContentAttachment else {
+                return
+        }
+        cdcontent.tempAttachment = image
+        cdcontent.uploadState = .start
+        cdmessage.content = cdcontent
+        cd.save()
+        
+        PrismCore.shared.getAttachmentURL(filename: imageName, conversationID: self.credential.conversationID, token: self.credential.accessToken) { (response, error) in
+            guard let imageData = UIImagePNGRepresentation(image),
+                let url = response?.uploadURL else {
+                    return
+            }
+            
+            var comp = URLComponents(url: url, resolvingAgainstBaseURL: false)
+            comp?.query = nil
+            comp?.fragment = nil
+            
+            guard let stringURL = comp?.url?.absoluteString else {
+                return
+            }
+            CacheVendor.shared.cacheImages.setObject(image, forKey: stringURL as NSString)
+            
+            content.url = stringURL
+            
+            cdcontent.url = stringURL
+            cdcontent.uploadState = .uploading
+            cdmessage.content = cdcontent
+            cd.save()
+            
+            PrismCore.shared.uploadAttachment(with: imageData, url: url, completionHandler: { (success, error) in
+                guard success else {
+                    return
+                }
+                cdcontent.uploadState = .finished
+                cdcontent.tempAttachment = nil
+                cdmessage.content = cdcontent
+                cd.save()
+                
+                message.content = content
+                
+                PrismCore.shared.publishMessage(topic: self.credential.topic, message: message, completionHandler: { (message, error) in
+                })
+            })
+        }
     }
     
     func sendMessage(sticker: StickerViewModel) {
         guard let content = ContentSticker(name: sticker.name,
                                            imageURL: sticker.imageURL.absoluteString,
                                            id: sticker.id,
-                                           packID: sticker.packID),
-            let message = message(with: content, type: .Sticker) else { return }
+                                           packID: sticker.packID) else { return }
+        let message = buildMessage(with: content, type: .Sticker)
         sendMessage(message: message)
     }
     
     private func sendMessage(message: Message) {
         //save to core data
-        coredata?.saveMessage(message: message, status: .pending)
+        coredata?.buildMessage(message: message, status: .pending)
+        coredata?.save()
         
         //publish to mqtt
         PrismCore.shared.publishMessage(topic: credential.topic, message: message) { (message, error) in }
     }
     
-    private func message(with content: MessageContentMappable, type: MessageType) -> Message? {
+    private func buildMessage(with content: MessageContentMappable, type: MessageType) -> Message {
         return Message(id: NSUUID().uuidString.lowercased(),
                        conversationID: credential.conversationID,
                        merchantID: credential.merchantID,
@@ -80,7 +130,8 @@ class ChatManager {
     
     @objc func chatReceived(sender: Notification) {
         guard let message = sender.object as? Message else { return }
-        coredata?.saveMessage(message: message, status: .sent)
+        coredata?.buildMessage(message: message, status: .sent)
+        coredata?.save()
     }
     
     @objc func chatDisconnect(sender: Notification) {

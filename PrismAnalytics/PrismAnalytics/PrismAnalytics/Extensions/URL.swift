30d2af8df31ffaaca36f2627d3088547737ce059
Fanni Suyuti <fannisuyuti@users.noreply.github.com>
2017-07-17 10:23:36 +0700

    [SDK-122] As an iOS UI SDK, I want to be able to send data conversation to Rover (#40)
    * Add rover related tracking code
    
    * no message
    
    * remove unused file
    
    * simplified code
    
    * no message
    
    * Call correct method
    
    * update publish message related code

---
+++ PrismAnalytics/PrismAnalytics/PrismAnalytics/Extensions/URL.swift
@@ -0,0 +1,44 @@
+//
+//  URL.swift
+//  PrismAnalytics
+//
+//  Created by fanni suyuti on 7/13/17.
+//  Copyright © 2017 PrismApp. All rights reserved.
+//
+
+import Foundation
+import PrismCore
+
+extension URL {
+    private static let PrismAPIBaseURLProduction = "https://api.prismapp.io/v2/metrics"
+    private static let PrismAPIBaseURLSandbox = "https://api.sandbox.prismapp.io/v2/metrics"
+    
+    private static var PrismAPIBaseURL: String {
+        get {
+            
+            guard let environment = PrismAnalytics.shared.environment else {
+                assert(PrismAnalytics.shared.environment != nil, "should configure PrismAnalytic first, call PrismAnalytics.shared.configure()")
+                return ""
+            }
+            
+            switch environment {
+            case .Production:
+                return PrismAPIBaseURLProduction
+            case .Sandbox:
+                return PrismAPIBaseURLSandbox
+            }
+        }
+    }
+    
+    static var ipifyAPI: URL {
+        get {
+            return URL(string: "https://api.ipify.org")!
+        }
+    }
+    
+    static var conversation: URL {
+        get {
+            return URL(string: PrismAPIBaseURL + "/conversations")!
+        }
+    }
+}

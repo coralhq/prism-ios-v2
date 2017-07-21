ffe9c6e2078713b1034e7a8a111ba087def0431b
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-07-21 08:05:13 +0700

    refine settings object

--- PrismUI/PrismUI/Components/Extensions/UIColor.swift
+++ PrismUI/PrismUI/Components/Extensions/UIColor.swift
@@ -13,18 +13,6 @@ extension UIColor {
         return UIColor(red: 87.0 / 255.0, green: 116.0 / 255.0, blue: 155.0 / 255.0, alpha: 1.0)
     }
     
-    class var steelBlueFill: UIColor {
-        return UIColor(red: 87.0 / 255.0, green: 116.0 / 255.0, blue: 155.0 / 255.0, alpha: 0.1)
-    }
-    
-    class var steelBlueMainViewFill: UIColor {
-        return steelBlue.withAlphaComponent(0.05)
-    }
-    
-    class var steelBlueBorder: UIColor {
-        return UIColor(red: 87.0 / 255.0, green: 116.0 / 255.0, blue: 155.0 / 255.0, alpha: 0.15)
-    }
-    
     class var tealish: UIColor {
         return UIColor(red: 38.0 / 255.0, green: 165.0 / 255.0, blue: 188.0 / 255.0, alpha: 1.0)
     }
@@ -37,76 +25,16 @@ extension UIColor {
         return UIColor(white: 50.0 / 255.0, alpha: 1.0)
     }
     
-    class var jetBlackMainViewFill: UIColor {
-        return jetBlack.withAlphaComponent(0.05)
-    }
-    
-    class var jetBlackFill: UIColor {
-        return UIColor(white: 50.0 / 255.0, alpha: 0.1)
-    }
-    
-    class var jetBlackBorder: UIColor {
-        return UIColor(white: 50.0 / 255.0, alpha: 0.15)
-    }
-    
     class var fadedRed: UIColor {
         return UIColor(red: 211.0 / 255.0, green: 56.0 / 255.0, blue: 93.0 / 255.0, alpha: 1.0)
-    }
-    
-    class var fadedRedMainViewFill: UIColor {
-        return fadedRed.withAlphaComponent(0.05)
-    }
-    
-    class var fadedRedFill: UIColor {
-        return UIColor(red: 211.0 / 255.0, green: 56.0 / 255.0, blue: 93.0 / 255.0, alpha: 0.1)
-    }
-    
-    class var fadedRedBorder: UIColor {
-        return UIColor(red: 211.0 / 255.0, green: 56.0 / 255.0, blue: 93.0 / 255.0, alpha: 0.15)
     }
     
     class var coolGreen: UIColor {
         return UIColor(red: 56.0 / 255.0, green: 185.0 / 255.0, blue: 100.0 / 255.0, alpha: 1.0)
     }
     
-    class var coolGreenMainViewFill: UIColor {
-        return coolGreen.withAlphaComponent(0.05)
-    }
-    
-    class var coolGreenFill: UIColor {
-        return UIColor(red: 56.0 / 255.0, green: 185.0 / 255.0, blue: 100.0 / 255.0, alpha: 0.1)
-    }
-    
-    class var coolGreenBorder: UIColor {
-        return UIColor(red: 56.0 / 255.0, green: 185.0 / 255.0, blue: 100.0 / 255.0, alpha: 0.15)
-    }
-    
     class var yellowOrange: UIColor {
         return UIColor(red: 255.0 / 255.0, green: 181.0 / 255.0, blue: 0.0, alpha: 1.0)
-    }
-    
-    class var yellowOrangeMainViewFill: UIColor {
-        return yellowOrange.withAlphaComponent(0.05)
-    }
-    
-    class var yellowOrangeFill: UIColor {
-        return UIColor(red: 255.0 / 255.0, green: 181.0 / 255.0, blue: 0.0, alpha: 0.1)
-    }
-    
-    class var yellowOrangeBorder: UIColor {
-        return UIColor(red: 255.0 / 255.0, green: 181.0 / 255.0, blue: 0.0, alpha: 0.15)
-    }
-    
-    class var tomatoFill: UIColor {
-        return UIColor(red: 236.0 / 255.0, green: 80.0 / 255.0, blue: 80.0 / 255.0, alpha: 0.1)
-    }
-    
-    class var tomatoMainViewFill: UIColor {
-        return tomato.withAlphaComponent(0.05)
-    }
-    
-    class var tomatoBorder: UIColor {
-        return UIColor(red: 236.0 / 255.0, green: 80.0 / 255.0, blue: 80.0 / 255.0, alpha: 0.15)
     }
     
     class var tomato: UIColor {
@@ -115,17 +43,5 @@ extension UIColor {
     
     class var darkSkyBlue: UIColor {
         return UIColor(red: 71.0 / 255.0, green: 187.0 / 255.0, blue: 231.0 / 255.0, alpha: 1.0)
-    }
-    
-    class var darkSkyBlueMainViewFill: UIColor {
-        return darkSkyBlue.withAlphaComponent(0.05)
-    }
-    
-    class var darkSkyBlueFill: UIColor {
-        return UIColor(red: 71.0 / 255.0, green: 187.0 / 255.0, blue: 231.0 / 255.0, alpha: 0.1)
-    }
-    
-    class var darkSkyBlueBorder: UIColor {
-        return UIColor(red: 71.0 / 255.0, green: 187.0 / 255.0, blue: 231.0 / 255.0, alpha: 0.15)
     }
 }
--- PrismUI/PrismUI/Components/Theme/Theme.swift
+++ PrismUI/PrismUI/Components/Theme/Theme.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-public enum ThemeOptions {
+public enum ThemeType {
     case AzureWhite
     case JetBlack
     case PinkScarlet
@@ -19,123 +19,81 @@ public enum ThemeOptions {
     case BananaTokyo
     
     public init(settings: [String: Any]) {
-        let rawValue = (settings["widget"] as! [String: Any])["style"] as! String        
+        let rawValue = (settings["widget"] as! [String: Any])["style"] as! String
         switch rawValue {
-        case "PINK": self = .PinkScarlet
-        case "BLACK": self = .JetBlack
-        case "WHITE": self = .AzureWhite
-        case "GREEN": self = .CitrusLime
-        case "RED": self = .CoralReef
-        case "YELLOW": self = .BananaTokyo
+        case "PINK":
+            self = .PinkScarlet
+            
+        case "BLACK":
+            self = .JetBlack
+            
+        case "WHITE":
+            self = .AzureWhite
+            
+        case "GREEN":
+            self = .CitrusLime
+            
+        case "RED":
+            self = .CoralReef
+            
+        case "YELLOW":
+            self = .BananaTokyo
+            
         case "BLUE": fallthrough
         default:
             self = .OceanBlue
         }
     }
+    
 }
 
 open class Theme {
-    open var navigationBarColor: UIColor
-    open var navigationBarTitleColor: UIColor
-    open var mainViewBackgroundColor: UIColor
-    open var chatSentBubbleBackgroundColor: UIColor
-    open var chatSentBubleBorderColor: UIColor
-    open var chatReceivedBubbleBackgroundColor: UIColor
-    open var chatReceivedBubleBorderColor: UIColor
-    open var chatTextColor: UIColor
-    open var chatSenderTextColor: UIColor
+    open var headerColor: UIColor
+    open var strokeColor: UIColor
+    open var buttonColor: UIColor
     
     init() {
-        navigationBarColor = UIColor.white
-        navigationBarTitleColor = UIColor.steelBlue
-        mainViewBackgroundColor = UIColor.steelBlueMainViewFill
-        chatSentBubbleBackgroundColor = UIColor.steelBlueFill
-        chatSentBubleBorderColor = UIColor.steelBlueBorder
-        chatReceivedBubbleBackgroundColor = UIColor.white
-        chatReceivedBubleBorderColor = UIColor.steelBlueBorder
-        chatTextColor = UIColor.jetBlack
-        chatSenderTextColor = UIColor.jetBlack
+        headerColor = UIColor.white
+        strokeColor = UIColor.steelBlue
+        buttonColor = UIColor.steelBlue
     }
     
-    func configure(option: ThemeOptions) {
+    func configure(option: ThemeType) {
         switch option {
         case .AzureWhite:
-            navigationBarColor = UIColor.white
-            navigationBarTitleColor = UIColor.steelBlue
-            mainViewBackgroundColor = UIColor.steelBlueMainViewFill
-            chatSentBubbleBackgroundColor = UIColor.steelBlueFill
-            chatSentBubleBorderColor = UIColor.steelBlueBorder
-            chatReceivedBubbleBackgroundColor = UIColor.white
-            chatReceivedBubleBorderColor = UIColor.steelBlueBorder
-            chatTextColor = UIColor.jetBlack
-            chatSenderTextColor = UIColor.jetBlack
+            headerColor = UIColor.white
+            strokeColor = UIColor.steelBlue
+            buttonColor = UIColor.steelBlue
             
         case .JetBlack:
-            navigationBarColor = UIColor.jetBlack
-            navigationBarTitleColor = UIColor.white
-            mainViewBackgroundColor = UIColor.jetBlackMainViewFill
-            chatSentBubbleBackgroundColor = UIColor.jetBlackFill
-            chatSentBubleBorderColor = UIColor.jetBlackBorder
-            chatReceivedBubbleBackgroundColor = UIColor.white
-            chatReceivedBubleBorderColor = UIColor.jetBlackBorder
-            chatTextColor = UIColor.jetBlack
-            chatSenderTextColor = UIColor.jetBlack
+            headerColor = UIColor.jetBlack
+            buttonColor = UIColor.jetBlack
+            strokeColor = UIColor.white
             
         case .PinkScarlet:
-            navigationBarColor = UIColor.fadedRed
-            navigationBarTitleColor = UIColor.white
-            mainViewBackgroundColor = UIColor.fadedRedMainViewFill
-            chatSentBubbleBackgroundColor = UIColor.fadedRedFill
-            chatSentBubleBorderColor = UIColor.fadedRedBorder
-            chatReceivedBubbleBackgroundColor = UIColor.white
-            chatReceivedBubleBorderColor = UIColor.fadedRedBorder
-            chatTextColor = UIColor.jetBlack
-            chatSenderTextColor = UIColor.jetBlack
+            headerColor = UIColor.fadedRed
+            buttonColor = UIColor.fadedRed
+            strokeColor = UIColor.white
             
         case .CitrusLime:
-            navigationBarColor = UIColor.coolGreen
-            navigationBarTitleColor = UIColor.white
-            mainViewBackgroundColor = UIColor.coolGreenMainViewFill
-            chatSentBubbleBackgroundColor = UIColor.coolGreenFill
-            chatSentBubleBorderColor = UIColor.coolGreenBorder
-            chatReceivedBubbleBackgroundColor = UIColor.white
-            chatReceivedBubleBorderColor = UIColor.coolGreenBorder
-            chatTextColor = UIColor.jetBlack
-            chatSenderTextColor = UIColor.jetBlack
+            headerColor = UIColor.coolGreen
+            buttonColor = UIColor.coolGreen
+            strokeColor = UIColor.white
             
         case .OceanBlue:
-            navigationBarColor = UIColor.darkSkyBlue
-            navigationBarTitleColor = UIColor.white
-            mainViewBackgroundColor = UIColor.darkSkyBlueMainViewFill
-            chatSentBubbleBackgroundColor = UIColor.darkSkyBlueFill
-            chatSentBubleBorderColor = UIColor.darkSkyBlueBorder
-            chatReceivedBubbleBackgroundColor = UIColor.white
-            chatReceivedBubleBorderColor = UIColor.darkSkyBlueBorder
-            chatTextColor = UIColor.jetBlack
-            chatSenderTextColor = UIColor.jetBlack
+            headerColor = UIColor.darkSkyBlue
+            buttonColor = UIColor.darkSkyBlue
+            strokeColor = UIColor.white
             
         case .CoralReef:
-            navigationBarColor = UIColor.tomato
-            navigationBarTitleColor = UIColor.white
-            mainViewBackgroundColor = UIColor.tomatoMainViewFill
-            chatSentBubbleBackgroundColor = UIColor.tomatoFill
-            chatSentBubleBorderColor = UIColor.tomatoBorder
-            chatReceivedBubbleBackgroundColor = UIColor.white
-            chatReceivedBubleBorderColor = UIColor.tomatoBorder
-            chatTextColor = UIColor.jetBlack
-            chatSenderTextColor = UIColor.jetBlack
+            headerColor = UIColor.tomato
+            buttonColor = UIColor.tomato
+            strokeColor = UIColor.white
             
         case .BananaTokyo:
-            navigationBarColor = UIColor.yellowOrange
-            navigationBarTitleColor = UIColor.white
-            mainViewBackgroundColor = UIColor.yellowOrangeMainViewFill
-            chatSentBubbleBackgroundColor = UIColor.yellowOrangeFill
-            chatSentBubleBorderColor = UIColor.yellowOrangeBorder
-            chatReceivedBubbleBackgroundColor = UIColor.white
-            chatReceivedBubleBorderColor = UIColor.yellowOrangeBorder
-            chatTextColor = UIColor.jetBlack
-            chatSenderTextColor = UIColor.jetBlack
-            
+            headerColor = UIColor.yellowOrange
+            buttonColor = UIColor.yellowOrange
+            strokeColor = UIColor.white
         }
     }
 }

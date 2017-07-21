6290cf8c57d6d254b81dabd7313bea92cdad1270
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-07-21 08:10:17 +0700

    add to navigation bar layout a headerview

--- PrismUI/PrismUI/Components/Extensions/UIViewController.swift
+++ PrismUI/PrismUI/Components/Extensions/UIViewController.swift
@@ -8,76 +8,6 @@
 
 import Foundation
 
-@objc internal protocol NavigationBarStyle {
-    @objc func navigationItemDidTapLeftBarButtonItem()
-    @objc func navigationItemDidTapRightBarButtonItem()
-}
-
-extension NavigationBarStyle where Self: UIViewController {
-    
-    func configureNavigationBar(title: String, subtitle: String, avatar: URL) {
-        navigationController?.setNavigationBarHidden(true, animated: false)
-        
-        let newNavBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 64))
-        
-        let backgroundView = UIView(frame: newNavBar.frame)
-        backgroundView.backgroundColor = Settings.shared.theme.navigationBarColor
-        newNavBar.addSubview(backgroundView)
-        
-        let item = UINavigationItem()
-        
-        let buttonImage = UIImage(named: "icBack", in: Bundle.init(identifier: "io.prismapp.PrismUI"), compatibleWith: nil)
-        let button = UIBarButtonItem(image: buttonImage, style: .plain, target: self, action: #selector(NavigationBarStyle.navigationItemDidTapLeftBarButtonItem))
-        button.title = ""
-        button.tintColor = .clear
-        
-        item.leftBarButtonItem = button
-        
-        let titleLabel = UILabel()
-        titleLabel.text = title
-        titleLabel.font = UIFont.navigationTitleFont()
-        titleLabel.textAlignment = .left
-        titleLabel.sizeToFit()
-        titleLabel.textColor = Settings.shared.theme.navigationBarTitleColor
-        
-        let subTitleLabel = UILabel()
-        subTitleLabel.text = subtitle
-        subTitleLabel.font = UIFont.navigationSubTitleFont()
-        subTitleLabel.textAlignment = .left
-        subTitleLabel.sizeToFit()
-        subTitleLabel.textColor = Settings.shared.theme.navigationBarTitleColor
-        
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 2, width: 32, height: 32))
-        imageView.downloadedFrom(url: avatar)
-        
-        let stackView = UIStackView(arrangedSubviews: [titleLabel, subTitleLabel])
-        stackView.distribution = .fillEqually
-        stackView.axis = .vertical
-        
-        stackView.frame = CGRect(x: 44, y: 0, width: UIScreen.main.bounds.width * 290/375, height: 35)
-        
-        titleLabel.sizeToFit()
-        subTitleLabel.sizeToFit()
-        
-        let titleView = UIView(frame: CGRect(x: 0, y: 0, width: stackView.bounds.width + 8 + imageView.bounds.width, height: 35))
-        titleView.addSubview(imageView)
-        titleView.addSubview(stackView)
-        
-        stackView.sizeToFit()
-        
-        item.titleView = titleView
-        
-        newNavBar.setItems([item], animated: false)
-        
-        view.addSubview(newNavBar)
-    }
-}
-
-extension UIViewController: NavigationBarStyle {
-    @objc func navigationItemDidTapRightBarButtonItem() {}
-    @objc func navigationItemDidTapLeftBarButtonItem() {}
-}
-
 extension UIViewController {
     func popErrorAlert(error: NSError) {
         let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
@@ -100,27 +30,6 @@ extension UIViewController {
         contentView.addSubview(vc.view)
         contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[view]-0-|", options: .init(rawValue: 0), metrics: nil, views: ["view":vc.view]))
         contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[view]-0-|", options: .init(rawValue: 0), metrics: nil, views: ["view":vc.view]))
-    }
-    
-    func replace(vc1: UIViewController?, with vc2: UIViewController?, animated: Bool) {
-        guard let vc2 = vc2 else { return }
-        let nvc = UINavigationController(rootViewController: vc2)
-        nvc.view.alpha = 0
-        
-        add(viewController: nvc, toView: view)
-        
-        if animated {
-            UIView.animate(withDuration: 0.25, animations: {
-                nvc.view.alpha = 1
-            }, completion: { (finished) in
-                guard let vc = vc1 else { return }
-                self.remove(viewController: vc)
-            })
-        } else {
-            nvc.view.alpha = 1
-            guard let vc = vc1 else { return }
-            remove(viewController: vc)
-        }
     }
     
     static var root: UIViewController? {
---
+++ PrismUI/PrismUI/Views/HeaderView.swift
@@ -0,0 +1,37 @@
+//
+//  HeaderView.swift
+//  PrismUI
+//
+//  Created by Nanang Rafsanjani on 7/20/17.
+//  Copyright © 2017 Prism. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var container: UIStackView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        imageView.layer.cornerRadius = imageView.frame.width/2
+        imageView.layer.masksToBounds = true
+    }
+    
+    func configure(settings: Settings) {
+        titleLabel.textColor = settings.theme.strokeColor
+        subtitleLabel.textColor = settings.theme.strokeColor
+        
+        titleLabel.text = settings.titleExpanded
+        subtitleLabel.text = settings.subtitle
+        
+        container.removeArrangedSubview(imageView)
+        if settings.persona.enabled {
+            container.insertArrangedSubview(imageView, at: 0)
+            imageView.downloadedFrom(url: settings.persona.imageURL)
+        }
+    }
+}

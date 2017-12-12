f76f4e9a6455e5966ccc76c50f6602f7c870a94a
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-12-12 11:32:49 +0700

    fix ui disable selection for this components

---
+++ PrismUI/PrismUI/Models/CoreData/Contents/CDContentAutoResponder.swift
@@ -0,0 +1,13 @@
+//
+//  CDContentAutoResponder.swift
+//  PrismUI
+//
+//  Created by Nanang Rafsanjani on 12/12/17.
+//  Copyright © 2017 Prism. All rights reserved.
+//
+
+import UIKit
+
+class CDContentAutoResponder: ValueTransformer {
+
+}
